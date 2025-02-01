5b73ccbcf2ca92cf5f83f7a50edf8868bb8becf6
imkrad <164296287+imkrad@users.noreply.github.com>
2025-02-01 14:25:24 +0800

    asda

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -1,11 +1,11 @@
 <template>
     <Head title="Users" />
-    <PageHeader title="Users" pageTitle="Lists" />
-    <div class="chat-wrapper d-lg-flex gap-1 mx-n4 mt-n4 p-1">
+    <!-- <PageHeader title="Users" pageTitle="Lists" /> -->
+    <!-- <div class="chat-wrapper d-lg-flex gap-1 mx-n4 mt-n4 p-1">
         <div class="file-manager-content w-100 p-4 pb-0" style="height: calc(100vh - 180px); overflow: auto;" ref="box">
             <Lists :statuses="statuses"/>
         </div>
-    </div>
+    </div> -->
 </template>
 <script>
 import Lists from './Components/Lists.vue';
